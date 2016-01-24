757528e6954bdb3ea9454684026372b0dd62eac2
Hawk30100 <hawk30100@yahoo.de>
2016-01-24 12:40:34 +0100

    Removed unused common.js

--- poker/public/javascript/common.js
+++
@@ -1,7 +0,0 @@
-$.get("/lobby", function(data) {
-    var players = data.split(" ");
-    players.splice(-1,1);
-    for(var i in players) {
-        $('#player-list').append($('<label><input type="checkbox">' + players[i] + '</label><br>'));
-    }
-});
